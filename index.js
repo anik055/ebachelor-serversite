7b8f2279a385470ba9d7ff98aac591ab2dc6208d
Md. Sajid K Jaman <59714409+anik055@users.noreply.github.com>
2022-03-29 12:18:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,48 @@ const ObjectID = require("mongodb").ObjectID;
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
-const uri =
-  "mongodb+srv://eBachelor:3ApXHf3IoUtd5zGI@cluster0.rzm4j.mongodb.net/bachelorCommerce?retryWrites=true&w=majority";
+// const uri =
+//   "mongodb+srv://eBachelor:3ApXHf3IoUtd5zGI@cluster0.rzm4j.mongodb.net/bachelorCommerce?retryWrites=true&w=majority";
 
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader("Acces-Control-Allow-Origin", "*");
-  res.setHeader("Acces-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.setHeader("Acces-Contorl-Allow-Methods", "Content-Type", "Authorization");
-  next();
-});
 
-app.use(bodyParser.json({ limit: "5mb" }));
+// app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "10mb" }));
 app.use(cors());
-
-const port = 8080;
+app.use(express.static("doctors"));
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.send("hello from db it's working working");
 });
+
+const uri =
+  "mongodb+srv://eBachelor:3ApXHf3IoUtd5zGI@cluster0.rzm4j.mongodb.net/bachelorCommerce?retryWrites=true&w=majority";
+
+// const app = express();
+// app.use((req, res, next) => {
+//   res.setHeader("Acces-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Acces-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//   res.setHeader("Acces-Contorl-Allow-Methods", "Content-Type", "Authorization");
+//   next();
+// });
+
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+
+// app.use(cors(corsOptions));
+
+// app.use(bodyParser.json({ limit: "10mb" }));
+// app.use(cors());
+
+const port = 5050;
+
+// app.get("/", (req, res) => {
+//   res.send("hello from db it's working working");
+// });
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -37,7 +60,6 @@ client.connect((err) => {
   const adminCollection = client.db("eBachelor").collection("admin");
 
   app.patch("/update/:id", (req, res) => {
-
     productCollection
       .updateOne(
         { _id: ObjectID(req.params.id) },
@@ -69,18 +91,23 @@ client.connect((err) => {
 
   app.get("/orders", (req, res) => {
     ordersCollection.find({}).toArray((err, documents) => {
-        res.send(documents);
-        // console.log("anik",documents);
+      res.send(documents);
     });
   });
 
-    app.post("/ordersByUser", (req, res) => {
-      const email = req.body.email;
-      // console.log("mail",email);
-      ordersCollection.find({ email: email }).toArray((err, documents) => {
-        res.send(documents);
-      });
+  app.post("/ordersByUser", (req, res) => {
+    const email = req.body.email;
+    ordersCollection.find({ email: email }).toArray((err, documents) => {
+      res.send(documents);
     });
+  });
+
+  app.post("/cart", (req, res) => {
+    const email = req.body.email;
+    cartCollection.find({ email: email }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
 
   app.get("/products/:category", (req, res) => {
     const category = req.params.category;
@@ -91,20 +118,18 @@ client.connect((err) => {
 
   app.post("/addAdmin", (req, res) => {
     const email = req.body;
-    console.log("admin",email)
     adminCollection.insertOne(email).then((result) => {
-      // console.log(result);
       res.send(result.insertedCount > 0);
     });
   });
 
   app.post("/isAdmin", (req, res) => {
-      const email = req.body.email;
-      console.log(email);
+    const email = req.body.email;
+    console.log("email",req.body);
     adminCollection.find({ email: email }).toArray((err, documents) => {
-        res.send(documents.length > 0);
-        console.log('doc',documents);
-        console.log(err);
+      res.send(documents.length > 0);
+      // console.log("doc", documents);
+      // console.log(err);
     });
   });
 
@@ -157,12 +182,7 @@ client.connect((err) => {
     });
   });
 
-  app.get("/cart", (req, res) => {
-    const search = req.query.search;
-    cartCollection.find().toArray((err, documents) => {
-      res.send(documents);
-    });
-  });
+
 
   app.delete("/delete/:id", (req, res) => {
     const id = ObjectID(req.params.id);
@@ -175,7 +195,6 @@ client.connect((err) => {
 
     cartCollection.findOneAndDelete({ _id: id }).then((documents) => {
       res.send(!!documents.value);
-
     });
   });
 });
